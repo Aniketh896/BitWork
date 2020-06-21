cb5951e82a7477e34fe7f48a16844ffa35792292
Anurag Sharan <sharananurag998@gmail.com>
2020-06-21 09:55:37 +0530

    Fixed CSS issue

--- BitWork/settings.py
+++ BitWork/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'BitWork')
+STATIC_ROOT = os.path.join(BASE_DIR, 'BitWork')
 
 ROOT_URLCONF = 'BitWork.urls'
 
